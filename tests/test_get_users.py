59fd7c178b235cb3158e7882266cc2e5ca1b3f89
Gopinath <jingadegopinathrao@gmail.com>
2026-03-26 17:49:11 +0530

    Added Allure live report

--- tests/test_get_users.py
+++ tests/test_get_users.py
@@ -1,24 +1,30 @@
-import pytest
+import allure
 from utils.api_client import get
 
 
-@pytest.mark.smoke
+@allure.feature("Users API")
+@allure.story("Get Users")
+# ✅ Add decorators HERE (above function)
+@allure.title("Verify GET Users API with valid response")
+@allure.description("This test verifies users API response")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("API", "Regression")
 def test_get_users():
-    """
-    Test to fetch users
-    """
+    # ✅ Step 1
+    with allure.step("Send GET request"):
+        response = get("/users")
 
-    response = get("/users")
+        # Attach response
+        allure.attach(
+            response.text,
+            name="Response Body",
+            attachment_type=allure.attachment_type.JSON
+        )
 
-    assert response.status_code == 200
+    # ✅ Step 2
+    with allure.step("Validate status code"):
+        assert response.status_code == 200
 
-    response_json = response.json()
-
-    assert isinstance(response_json, list)
-    assert len(response_json) > 0
-
-    first_user = response_json[0]
-
-    assert "id" in first_user
-    assert "name" in first_user
-    assert "email" in first_user
+    # ✅ Step 3
+    with allure.step("Validate response is not empty"):
+        assert len(response.json()) > 0
